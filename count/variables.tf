c00b642aba50a6f372654f9af13b5079d1575c7c
Ram-eng01 <rammohan63025@gmail.com>
2025-06-09 12:36:03 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,17 @@
+variable "image_id" {
+    type = string  
+    default = "ami-090252cbe067a9e58"
+    description = "RHE-9 AMI ID"
+
+}
+
+
+variable "instancetype" {
+    default = "t3.micro"
+    type = string
+}
+
+variable "instance_names" {
+    type = list
+    default = ["db","backned","frontend"]
+}
