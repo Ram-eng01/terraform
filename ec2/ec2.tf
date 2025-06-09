cf4347eeb58be54cda941d7ac0c28e2bbe21e4f4
Ram-eng01 <rammohan63025@gmail.com>
2025-06-09 11:19:17 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "db" {
 
     ami = "ami-090252cbe067a9e58"
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
 
     tags = {
         Name = "db"
