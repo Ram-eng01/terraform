a10c66b751d38af858af38383c331c5b8fa297bf
Ram-eng01 <rammohan63025@gmail.com>
2025-06-26 11:46:56 +0530

    terraform

---
+++ aws-vpc/vpc.tf
@@ -0,0 +1,45 @@
+resource "aws_vpc" "main" {
+    cidr_block = var.vpc_cidr
+    instance_tenancy = "default"
+    enable_dns_hostnames = var.enable_dns_hostnames
+
+
+    tags = merge(
+        var.common_tags,
+        var.vpc_tags,
+        {
+            Name = local.resource_name
+        }
+    )
+
+}
+
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = merge(
+        var.common_tags,
+        var.igw_tags,
+        {
+            Name = local.resource_name
+        }
+    )
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  availability_zone = local.az_names[count.index]
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = "true"
+
+  tags = merge(
+    var.common_tags,
+    var.public_subnet_cidrs_tags,
+    {
+        Name = "${local.resource_name}-${local.az_names[count.index]}"
+    }
+  )
+}
+
