2e19d8a871877377edce4bce65f5f9a28bbb36da
Ram-eng01 <rammohan63025@gmail.com>
2025-06-28 10:48:38 +0530

    terraform

--- aws-vpc/variable.tf
+++ aws-vpc/variable.tf
@@ -50,3 +50,63 @@ variable "public_subnet_cidrs_tags" {
      type = map 
      default = {}
 }
+
+
+##3 private subnet #####
+
+variable "private_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.private_subnet_cidrs)  == 2
+      error_message = "please provide 2 valid pubkic subnet CIDR "
+    }
+    
+}
+
+variable "private_subnet_cidrs_tags" {
+     type = map 
+     default = {}
+}
+
+### database subnet #######
+
+variable "database_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.database_subnet_cidrs)  == 2
+      error_message = "please provide 2 valid pubkic subnet CIDR "
+    }
+    
+}
+
+variable "database_subnet_cidrs_tags" {
+     type = map 
+     default = {}
+}
+
+
+### natgateway
+
+variable "natgateway_tags" {
+    type = map 
+    default = {}
+}
+
+
+####
+
+variable "public_route_tabe_tags" {
+    type = map 
+    default = {}
+}
+
+variable "private_route_tabe_tags" {
+    type = map 
+    default = {}
+}
+
+
+variable "database_route_tabe_tags" {
+    type = map 
+    default = {}
+}
