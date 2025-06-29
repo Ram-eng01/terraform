a8b4a962edf4ebdcfb97a117e242bbe2bd7c0bd1
Ram-eng01 <rammohan63025@gmail.com>
2025-06-29 19:13:16 +0530

    k8-expense

--- Expense_Project_Dev/03-bastion/main.tf
+++ Expense_Project_Dev/03-bastion/main.tf
@@ -1,14 +1,17 @@
 module "bastion" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "single-instance"
+  name = "${var.project_name}-${var.environment}-bastion"
 
   instance_type = "t2.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg_id.value]
   
-  subnet_id     = "subnet-eddcdzz4"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  subnet_id     = local.public_subnet_id
+  ami = data.aws_ami.ami_info.id
+  tags = merge(
+    var.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}-bastion"
+    }
+  )
 }
